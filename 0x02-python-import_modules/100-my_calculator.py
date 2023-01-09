b6780f9209bd0db33825db30f2e1020cc0164b4f
Kekeli92 <lytqua@gmail.com>
2023-01-09 12:30:38 +0000

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+import sys
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    print("{}".format(a))
+
+    # Check if there are three command line arguments
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    # Dictionary mapping operator values to functions
+    op_func = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+        }
+
+    # Get operands and operator
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+
+    # Check if operator is valid
+    if op not in op_func:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # Perform operation and print result
+    result = op_func[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
